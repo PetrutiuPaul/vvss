66bbeb6e65fc30c0078553d62d26a09340d6dda3
Paul <paul970410@gmail.com>
2019-04-01 08:22:34 +0300

    add tests, lab 2

--- src/test/java/ppir2169MV/AppTest.java
+++ src/test/java/ppir2169MV/AppTest.java
@@ -1,20 +1,151 @@
 package ppir2169MV;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
+import ppir2169MV.control.BibliotecaCtrl;
+import ppir2169MV.model.Carte;
+import ppir2169MV.repository.repoInterfaces.CartiRepoInterface;
+import ppir2169MV.repository.repoMock.CartiRepoMock;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
+    CartiRepoInterface cr = new CartiRepoMock();
+    BibliotecaCtrl bc = new BibliotecaCtrl(cr);
+
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+
+    @Test
+    public void TC1_ECP()
+    {
+        Carte c = new Carte();
+        try {
+            c.setTitlu("Ion");
+            c.setAnAparitie("1919");
+            c.adaugaReferent("Rebreanu");
+            c.adaugaCuvantCheie("sapa");
+            c.adaugaCuvantCheie("mort");
+
+            bc.adaugaCarte(c);
+        }catch (Exception ex){
+            fail();
+        }
+
+        assertTrue( true );
+    }
+
+    @Test
+    public void TC2_ECP()
+    {
+        Carte c = new Carte();
+        try {
+            c.setTitlu("Moara noroc");
+            c.setAnAparitie("-1");
+            c.adaugaReferent("Nimeni");
+            c.adaugaCuvantCheie("doamne");
+
+            c.adaugaCuvantCheie("ajuta");
+
+            bc.adaugaCarte(c);
+
+            fail();
+        }catch (Exception ex){
+            if(ex.getMessage().equals("Editura invalid!"))
+                assertTrue(true);
+            else
+                fail();
+        }
+    }
+
+    @Test
+    public void TC3_ECP()
+    {
+        Carte c = new Carte();
+        try {
+            c.setTitlu("");
+            c.setAnAparitie("2010");
+            c.adaugaReferent("da");
+            c.adaugaCuvantCheie("amin");
+
+            bc.adaugaCarte(c);
+
+            fail();
+        }catch (Exception ex){
+            if(ex.getMessage().equals("Titlu invalid!"))
+                assertTrue(true);
+            else
+                fail();
+        }
+    }
+
+    @Test
+    public void TC4_ECP()
+    {
+        Carte c = new Carte();
+        try {
+            c.setTitlu("Moara ghinion");
+            c.setAnAparitie("2010");
+            c.adaugaReferent("2");
+            c.adaugaCuvantCheie("amin");
+
+            bc.adaugaCarte(c);
+
+            fail();
+        }catch (Exception ex){
+            if(ex.getMessage().equals("Autor invalid!"))
+                assertTrue(true);
+            else
+                fail();
+        }
+    }
+
+    @Test
+    public void TC1_BVA()
+    {
+        Carte c = new Carte();
+        try {
+            c.setTitlu("nu");
+            c.setAnAparitie("2019");
+            c.adaugaReferent("da");
+            c.adaugaCuvantCheie("nou");
+
+            bc.adaugaCarte(c);
+
+            assertTrue(true);
+        }catch (Exception ex){
+                fail();
+        }
+    }
+
+    @Test
+    public void TC2_BVA()
+    {
+        Carte c = new Carte();
+        try {
+            c.setTitlu("mara are mere");
+            c.setAnAparitie("2019");
+            c.adaugaReferent("autor1");
+            c.adaugaReferent("autor2");
+            c.adaugaCuvantCheie("amin");
+
+            bc.adaugaCarte(c);
+
+            assertTrue(true);
+        }catch (Exception ex){
+            if(ex.getMessage().equals("Titlu invalid!"))
+                assertTrue(true);
+            else
+                fail();
+        }
     }
 }
